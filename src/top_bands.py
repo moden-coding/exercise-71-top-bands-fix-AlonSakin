430f04f8eee3bfca9a7ac4f1d2939e0998cb6e04
AlonSakin <29asakin@gannacademy.org>
2025-11-07 09:52:50 -0500

    #!/usr/bin/env python3
    import pandas as pd
    
    def top_bands():
        top = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
        bands = pd.read_csv("src/bands.tsv", sep="\t")
    
        bands = bands.apply(lambda s: s.str.upper())
    
        return pd.merge(top, bands, left_on="Artist", right_on="Band")
    
    def main():
        print(top_bands())
    
    if __name__ == "__main__":
        main()

--- src/top_bands.py
+++ src/top_bands.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def top_bands():
-    pass
+    top = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    bands = pd.read_csv("src/bands.tsv", sep="\t")
+
+    bands = bands.apply(lambda s: s.str.upper())
+
+    return pd.merge(top, bands, left_on="Artist", right_on="Band")
 
 def main():
-    top_bands()
+    print(top_bands())
 
 if __name__ == "__main__":
     main()
